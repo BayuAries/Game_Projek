a7f7293a128d0f794254c21a0a663e63edca2bc8
BayuAries <62095092+BayuAries@users.noreply.github.com>
2020-06-26 19:17:26 +0700

    respawn advtr

---
+++ Sneakers King CP Project/Assets/Scripts/Kill Fall/KillFloor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillFloor : MonoBehaviour
+{
+    [SerializeField]Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.transform.CompareTag("Player"))
+            col.transform.position = spawnPoint.position;
+    }
+}
