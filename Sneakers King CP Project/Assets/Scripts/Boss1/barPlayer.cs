10c943d55d49b9f21382c6d5a8a0939c99284030
Nvlhnn <hanannaufal17@gmail.com>
2020-06-29 23:47:07 +0700

    Add damage boss1 for player and Player's health bar

---
+++ Sneakers King CP Project/Assets/Scripts/Boss1/barPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class barPlayer : MonoBehaviour
+{
+	public Players players;
+	public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+	void Start()
+	{
+		slider.maxValue = players.health;
+        slider.value = players.health;
+        fill.color = gradient.Evaluate(1f);
+        Debug.Log(slider.value);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		slider.value = players.health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+
+
+        Debug.Log(slider.value);
+    }
+}
