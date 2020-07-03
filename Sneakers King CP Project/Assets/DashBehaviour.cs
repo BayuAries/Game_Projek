3bf2f6e0461fccb65a6a243372bd56b3d6dde750
HusniHeartnet <66942561+HusniHeartnet@users.noreply.github.com>
2020-07-03 15:32:23 +0700

    Boss2 Animation+Movement Finished

---
+++ Sneakers King CP Project/Assets/DashBehaviour.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashBehaviour : StateMachineBehaviour
+{
+    private float timer;
+    public float minTime;
+    public float maxTime;
+    [SerializeField]
+    Transform pointA, pointB;
+    Rigidbody2D rb;
+    private Transform playerPos;
+    public float speed;
+    float DashDirection;
+    Vector3 currentTarget;
+    bool destinationReached = false;
+    SpriteRenderer sprite;
+    SpriteRenderer effectsprite;
+    SpriteRenderer effectsprite2;
+    Animator dasheffect;
+    Animator dasheffect2;
+    Animator Aura;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        timer = Random.Range(minTime, maxTime);
+        rb = GameObject.FindWithTag("Boss2").GetComponentInChildren<Rigidbody2D>();
+        pointA = GameObject.FindWithTag("PointA").GetComponent<Transform>();
+        sprite = GameObject.FindWithTag("Boss2").GetComponent<SpriteRenderer>();
+        pointB = GameObject.FindWithTag("PointB").GetComponent<Transform>();
+        effectsprite = GameObject.FindWithTag("Boss2").transform.GetChild(0).GetComponent<SpriteRenderer>();
+        effectsprite2 = GameObject.FindWithTag("Boss2").transform.GetChild(1).GetComponent<SpriteRenderer>();
+        dasheffect = GameObject.FindWithTag("Boss2").transform.GetChild(0).GetComponent<Animator>();
+        dasheffect2 = GameObject.FindWithTag("Boss2").transform.GetChild(1).GetComponent<Animator>();
+        Aura = GameObject.FindWithTag("Boss2").transform.GetChild(2).GetComponent<Animator>();
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+        DashDirection = rb.position.x - playerPos.position.x;
+        if (rb.position.x == pointA.position.x)
+        {
+            currentTarget.x = pointB.position.x;
+            sprite.flipX = true;
+            effectsprite.flipX = true;
+            effectsprite2.flipX = true;
+            dasheffect.SetTrigger("Dash");
+            dasheffect2.SetTrigger("Dash");
+            Aura.SetBool("ON", false);
+        }
+        else if (rb.position.x == pointB.position.x)
+        {
+            currentTarget.x = pointA.position.x;
+            sprite.flipX = false;
+            effectsprite.flipX = false;
+            effectsprite2.flipX = false;
+            dasheffect.SetTrigger("Dash");
+            dasheffect2.SetTrigger("Dash");
+            Aura.SetBool("ON", false);
+        }
+
+        Debug.Log(currentTarget);
+        
+        rb.transform.position = Vector3.MoveTowards(rb.transform.position, new Vector3(currentTarget.x, rb.transform.position.y, rb.transform.position.z), speed * Time.deltaTime);
+        if (timer <= 0)
+        {
+            animator.SetTrigger("idle");
+            dasheffect.ResetTrigger("Dash");
+            dasheffect2.ResetTrigger("Dash");
+            Aura.SetBool("ON", true);
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
--- Sneakers King CP Project/Assets/Scripts/Boss2/Boss2.cs
+++ Sneakers King CP Project/Assets/Scripts/Boss2/Boss2.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Boss2 : MonoBehaviour
 {
+    public int health;
+    public int damage;
+    private float timeBtwDamage = 1.5f;
+
+    private Animator anim;
+    public Animator camAnim;
+    public Slider slider;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponentInChildren<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (health <= 250)
+        {
+            //anim.SetTrigger("stageTwo");
+        }
+
+        if (health <= 0)
+        {
+            //anim.SetTrigger("death");
+        }
+        //slider.value = health;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //deal player damage
+        if (other.CompareTag("Player")){
+            if (timeBtwDamage <= 0)
+            {
+                camAnim.SetTrigger("shake");
+                other.GetComponent<PlayerAdventure>().health -= damage;
+            }
+        }
     }
 }
