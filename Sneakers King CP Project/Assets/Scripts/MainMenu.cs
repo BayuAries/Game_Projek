0333ba1e310e84cb48698c49bfee2a738675b235
Ferdian337 <18523149@students.uii.ac.id>
2020-07-04 18:22:17 +0700

    Main Menu
    Perbaikin Main Menu

--- Sneakers King CP Project/Assets/Scripts/MainMenu.cs
+++ Sneakers King CP Project/Assets/Scripts/MainMenu.cs
@@ -6,9 +6,14 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void StartButton()
+    public void PvpButton()
     {
         SceneManager.LoadScene("PVP");
+    }
+
+     public void StoryButton()
+    {
+        SceneManager.LoadScene("ADVENTURE");
     }
 
     // Update is called once per frame
