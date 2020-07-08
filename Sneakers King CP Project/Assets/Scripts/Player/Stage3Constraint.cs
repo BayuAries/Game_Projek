62a73a25007cb82ed79edc8780277353961ffc51
HusniHeartnet <66942561+HusniHeartnet@users.noreply.github.com>
2020-07-09 01:00:09 +0700

    timeline3dongs

---
+++ Sneakers King CP Project/Assets/Scripts/Player/Stage3Constraint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage3Constraint : MonoBehaviour
+{
+    Transform playerPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPos = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerPos.position.x < -8.5f)
+        {
+            playerPos.transform.position = new Vector3(-8.5f, playerPos.position.y, playerPos.position.z);
+        }
+        else if (playerPos.position.x > 8.5)
+        {
+            playerPos.transform.position = new Vector3(8.5f, playerPos.position.y, playerPos.position.z);
+        }
+    }
+}
